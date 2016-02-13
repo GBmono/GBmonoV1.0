10b0cc1224e3f9b84184be80ebfeede11099d302
Justin Li <blurlzy@hotmail.com>
2016-02-13 23:24:54 +1100

    added dlls

--- src/Web Api/Gbmono.Api.Admin/App_Start/WebApiConfig.cs
+++ src/Web Api/Gbmono.Api.Admin/App_Start/WebApiConfig.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+
 namespace Gbmono.Api.Admin
 {
     public static class WebApiConfig
@@ -10,6 +14,21 @@ namespace Gbmono.Api.Admin
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            // config.SuppressDefaultHostAuthentication();
+            // Bearer token authentication
+            // config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Global error handling
+            // exception logger
+            // There can be multiple exception loggers. (By default, no exception loggers are registered.)
+            // we register exception logger here
+            // config.Services.Add(typeof(IExceptionLogger), new GenericExceptionLogger());
+
+            // There must be exactly one exception handler. (There is a default one that may be replaced.)
+            // To make this sample easier to run in a browser, replace the default exception handler with one that sends
+            // back text/plain content for all errors.
+            // config.Services.Replace(typeof(IExceptionHandler), new GenericExceptionHandler());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -19,6 +38,22 @@ namespace Gbmono.Api.Admin
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // using camel casing for property names
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            // ignore reference loop handling
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+            // remove default XML hanlder
+            var matches = config.Formatters
+                               .Where(f => f.SupportedMediaTypes.Where(
+                                m => m.MediaType.ToString() == "application/xml" || m.MediaType.ToString() == "text/xml").Count() > 0)
+                               .ToList();
+
+            foreach (var match in matches)
+            {
+                config.Formatters.Remove(match);    // remove
+            }
         }
     }
 }
