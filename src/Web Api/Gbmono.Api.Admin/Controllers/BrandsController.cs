914ae8a9c33f15678a62369a28dbb15e61cb88b5
Justin Li <blurlzy@hotmail.com>
2016-02-13 22:57:24 +1100

    add brand controller

---
+++ src/Web Api/Gbmono.Api.Admin/Controllers/BrandsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Gbmono.EF.Models;
+using Gbmono.EF.Infrastructure;
+
+namespace Gbmono.Api.Admin.Controllers
+{
+    [RoutePrefix("Brands")]
+    public class BrandsController : ApiController
+    {
+        private readonly RepositoryManager _repositoryManager;
+
+        #region ctor
+        public BrandsController()
+        {
+            _repositoryManager = new RepositoryManager();
+        }
+        #endregion
+
+        public IEnumerable<Brand> GetAll()
+        {
+            return _repositoryManager.BrandRepository.Table.OrderBy(m => m.Name).ToList();
+        }
+    }
+}
