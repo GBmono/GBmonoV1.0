8a5ffa177ce3b4de1027acd587b4ce9eafa16686
Justin Li <blurlzy@hotmail.com>
2016-02-14 20:35:29 +1100

    temp angular app in api project

---
+++ src/Web Api/Gbmono.Api.Admin/temp/angularApp/layout.js
@@ -0,0 +1,23 @@
+﻿/*
+    layout controller
+*/
+(function (module) {
+    // inject the controller params
+    ctrl.$inject = [];
+
+    // create controller
+    module.controller('layoutController', ctrl);
+
+    // controller body
+    function ctrl() {
+
+    }
+})(angular.module('gbmono'));
+
+
+
+
+
+
+
+
