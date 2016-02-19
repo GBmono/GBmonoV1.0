17025840e300fada14a6a91fc3239b2235b3fa44
allenyinsh <429616790@qq.com>
2016-02-20 01:46:55 +0800

    file

---
+++ src/Shared/Gbmono.Common/StringHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gbmono.Common
+{
+    public static class StringHelper
+    {
+        public static string RemoveEmptyOrWrapCharacters(this string input)
+        {
+            return input.Replace("\r", "").Replace("\n", "").Trim();
+        }
+
+        public static string ToDBC(this string input)
+        {
+            char[] c = input.ToCharArray();
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (c[i] == 12288)
+                {
+                    c[i] = (char)32;
+                    continue;
+                }
+                if (c[i] > 65280 && c[i] < 65375)
+                    c[i] = (char)(c[i] - 65248);
+            }
+            return new string(c);
+        }
+    }
+}
