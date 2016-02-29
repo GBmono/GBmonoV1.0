5773b13c77bfe55121c57817a8c580fc3adb2c54
Justin Li <blurlzy@hotmail.com>
2016-02-29 22:03:05 +1100

    model changes

--- src/Shared/Gbmono.EF/Models/ProductEvent.cs
+++ src/Shared/Gbmono.EF/Models/ProductEvent.cs
@@ -5,14 +5,14 @@ namespace Gbmono.EF.Models
     /// <summary>
     /// 商品统计模型 (包括产品点击测试，被扫描次数等)
     /// </summary>
-    public class ProductStats
+    public class ProductEvent
     {
-        public int ProductStatsId { get; set; }
+        public int ProductEventId { get; set; }
 
         public int ProductId { get; set; }
         public Product Product { get; set; }
 
-        public short StatsType { get; set; }
+        public short EventTypeId { get; set; }
 
         public string UserId { get; set; }
 
---
+++ src/Shared/Gbmono.EF/Models/ProductEventType.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gbmono.EF.Models
+{
+    public enum ProductEventType
+    {
+        Scan = 1, // 扫描条形码
+
+        View = 2 // 访问产品
+    }
+}
