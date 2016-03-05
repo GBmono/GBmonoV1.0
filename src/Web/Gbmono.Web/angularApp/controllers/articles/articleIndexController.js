2e168c157b41fbf14c0dcc3d795d1c3fb4549d5e
Justin Li <blurlzy@hotmail.com>
2016-03-05 23:15:54 +1100

    register, article module

---
+++ src/Web/Gbmono.Web/angularApp/controllers/articles/articleIndexController.js
@@ -0,0 +1,32 @@
+﻿/*
+    profile controller
+*/
+(function (module) {
+    // inject the controller params
+    ctrl.$inject = ['$location', 'utilService'];
+
+    // create controller
+    module.controller('articleIndexController', ctrl);
+
+    // controller body
+    function ctrl($location, utilService) {
+        var vm = this;
+        
+        // product image root path
+        vm.imgRoot = gbmono.img_root_path;
+        // paging
+        vm.paging = { pageIndex: 1, pageSize: 12 };
+        // indicate if all data is loaded
+        vm.isAllDataLoaded = false;
+
+
+        init(); // page init 
+
+        function init() {
+
+        }
+
+
+
+    }
+})(angular.module('gbmono'));
