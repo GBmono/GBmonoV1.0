49b83f08191397ee81f9479f26d476698bb06787
Justin Li <blurlzy@hotmail.com>
2016-04-03 21:43:25 +1000

    add chinese convert library

---
+++ src/Utils/Gbmono.Utils.Chinese/ChineseConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Gbmono.Utils.Chinese
+{
+    public class ChineseConverter
+    {
+        public static string ToSimplifiedChinese(string str)
+        {
+            return Microsoft.VisualBasic.Strings.StrConv(str, Microsoft.VisualBasic.VbStrConv.SimplifiedChinese, 0);
+        }
+
+        public static string ToTraditionalChinese(string str)
+        {
+            return Microsoft.VisualBasic.Strings.StrConv(str, Microsoft.VisualBasic.VbStrConv.TraditionalChinese, 0);
+        }
+    }
+}
