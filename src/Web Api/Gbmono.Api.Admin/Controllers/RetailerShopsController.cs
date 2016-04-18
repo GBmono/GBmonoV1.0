39f324038cad86096b0fdd5c4cdc906069911015
allenyinsh <429616790@qq.com>
2016-04-18 21:57:52 +0800

    retailer shop

---
+++ src/Web Api/Gbmono.Api.Admin/Controllers/RetailerShopsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Gbmono.EF.Infrastructure;
+using Gbmono.EF.Models;
+
+namespace Gbmono.Api.Admin.Controllers
+{
+    [RoutePrefix("api/RetailerShops")]
+    public class RetailerShopsController : ApiController
+    {
+        private readonly RepositoryManager _repositoryManager;
+
+        #region ctor
+        public RetailerShopsController()
+        {
+            _repositoryManager = new RepositoryManager();
+        }
+        #endregion
+
+        [AllowAnonymous]
+        public async Task<IEnumerable<RetailerShop>> GetByRetailerId(int id)
+        {
+            return await _repositoryManager.RetailerShopRepository
+                                     .Table
+                                     .Where(m => m.RetailerId == id)
+                                     .ToListAsync();
+        }
+
+    }
+}
--- src/Web/Gbmono.Web.Admin/angularApp/controllers/retailers/retailerShopController.js
+++ src/Web/Gbmono.Web.Admin/angularApp/controllers/retailers/retailerShopController.js
@@ -3,23 +3,85 @@
 */
 (function (module) {
     // inject the controller params
-    ctrl.$inject = ['$scope'];
+    ctrl.$inject = ['$scope',
+                    '$routeParams',
+                    'pluginService',
+                    'retailerShopsDataFactory'];
 
     // create controller
     module.controller('retailerShopListController', ctrl);
 
     // controller body
-    function ctrl($scope) {
+    function ctrl($scope, $routeParams, pluginService, retailerShopsDataFactory) {
+
+        //retailerId
+        var retailerId = $routeParams.retailerId ? parseInt($routeParams.retailerId) : 0;
 
         // page init
         init();
 
+
+        // update event handelr
+
         function init() {
-
+            bindShops(retailerId);
         }
 
-        function bindShopGrid() {
-
+        // retreive brand data and binding it into kendo grid
+        function bindShops() {
+            // init kendo ui grid with brand data
+            $scope.mainGridOptions = {
+                dataSource: {
+                    transport: {
+                        read: function (e) {
+                            retailerShopsDataFactory.getByShopsByRetailerId(retailerId)
+                                .success(function (data) {
+                                    e.success(data);
+                                });
+                        }
+                    }
+                },
+                pageable: {
+                    numeric: false,
+                    previousNext: false,
+                    messages: {
+                        display: "总计: {2}"
+                    }
+                },
+                sortable: true,
+                height: 650,
+                filterable: false,
+                columns: [
+                    {
+                        field: "name", title: "名称", width: 250,
+                        template: '<i class="ace-icon fa fa-caret-right blue"></i> <a class="grey" ng-href="\\#/retailerShop/#=retailShopId#/second">#= name #</a>',
+                    },
+                    {
+                        field: "displayName", title: "显示名", width: 250,
+                    },
+                    {
+                        field: "address", title: "地址", width: 250
+                    },
+                    {
+                        field: "phone", title: "电话", width: 150
+                    },
+                    {
+                        field: "taxFree", title: "免税", width: 80,
+                        template: "<input type='checkbox' disabled='true'  data-bind='checked: taxFree' #= taxFree ? checked='checked' : '' #/>",
+                    },
+                    {
+                        field: "unionpay", title: "银联", width: 80,
+                        template: "<input type='checkbox' disabled='true' data-bind='checked: unionpay' #= unionpay ? checked='checked' : '' #/>",
+                    },
+                    {
+                        field: "enabled", title: "激活", width: 80,
+                        template: "<input type='checkbox' disabled='true' data-bind='checked: enabled' #= enabled ? checked='checked' : '' #/>",
+                    }
+                    
+                ]
+            };
         }
+
+
     }
 })(angular.module('gbmono'));
