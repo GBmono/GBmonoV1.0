322dd038c8a4e885f57e2c7d052dee33a33faa83
raphael-yao <imbaygb@live.cn>
2016-04-18 23:05:57 +0800

    add RouteGeneric

---
+++ src/Shared/Gbmono.Search.IndexManager/Models/RouteGeneric.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gbmono.Search.IndexManager.Models
+{
+    public class RouteGeneric<T> where T : class
+    {
+        public T Doc { get; set; }
+
+        public string RouteName { get; set; }
+    }
+}
