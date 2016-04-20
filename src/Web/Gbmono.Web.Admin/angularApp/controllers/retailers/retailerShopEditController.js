6923039419fdd19a3d5f15687b5a90970be3a9a5
allenyinsh <429616790@qq.com>
2016-04-21 00:17:12 +0800

    edit page

--- src/Web/Gbmono.Web.Admin/angularApp/controllers/retailers/retailerShopEditController.js
+++ src/Web/Gbmono.Web.Admin/angularApp/controllers/retailers/retailerShopEditController.js
@@ -3,24 +3,57 @@
 */
 (function (module) {
     // inject the controller params
-    ctrl.$inject = ['$scope', '$routeParams', 'pluginService', 'retailerShopDataFactory'];
+    ctrl.$inject = [
+        '$scope', '$routeParams', 'pluginService', 'retailerShopDataFactory', 'locationDataFactory'];
 
     // create controller
     module.controller('retailerShopEditController', ctrl);
 
     // controller body
-    function ctrl($scope, $routeParams, pluginService, retailerShopDataFactory) {
+    function ctrl($scope, $routeParams, pluginService, retailerShopDataFactory, locationDataFactory) {
         var retailerShopId = $routeParams.retailerShopId ? parseInt($routeParams.retailerShopId) : 0;
-
+        $scope.editShopStateId = null;
+        // states
+        $scope.states = [];
+        // cities
+        $scope.cities = [];
 
         // page init
         init();
 
-
-
         function init() {
+            getStates(1);
+
             getRetailerShop(retailerShopId);
         }
+
+        // load cities by state for edit
+        $scope.stateChangedForEdit = function () {
+            getCitiesForEdit($scope.editShopStateId);
+        };
+
+        // load states
+        function getStates(countryId) {
+            locationDataFactory.getStates(countryId)
+                .success(function (data) {
+                    $scope.states = data;
+                });
+        }
+
+        // load cities for edit
+        function getCitiesForEdit(stateId) {
+            if (stateId) {
+                locationDataFactory.getCities(stateId)
+                    .success(function (data) {
+                        $scope.citiesForEdit = data;
+                    });
+            } else {
+                $scope.citiesForEdit = [];
+            }
+        }
+
+      
+
 
         function getRetailerShop(id) {
             retailerShopDataFactory.getById(id)
