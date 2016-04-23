4ee698aed89bd5789ef6c3bb33a8b2883bc03777
raphael-yao <imbaygb@live.cn>
2016-04-23 20:33:04 +0800

    add retailshop search request

---
+++ src/Shared/Gbmono.Search.ViewModel/Requests/RetailShopSearchRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gbmono.Search.ViewModel.Requests
+{
+    public class RetailShopSearchRequest
+    {
+        public int? RetailerId { get; set; }
+        public string Keyword { get; set; }
+    }
+}
