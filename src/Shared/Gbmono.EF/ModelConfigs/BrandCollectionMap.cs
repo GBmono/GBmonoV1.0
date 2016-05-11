48626f5766b1420194a226847135b7c88e8ced4b
Justin Li <blurlzy@hotmail.com>
2016-05-11 14:18:27 +1000

    add brand colleciton models

---
+++ src/Shared/Gbmono.EF/ModelConfigs/BrandCollectionMap.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Gbmono.EF.Models;
+
+namespace Gbmono.EF.ModelConfigs
+{
+    public class BrandCollectionMap: EntityTypeConfiguration<BrandCollection>
+    {
+        public BrandCollectionMap()
+        {
+            ToTable("BrandCollection");
+
+            HasKey(m => m.BrandCollectionId);
+        }
+    }
+}
---
+++ src/Shared/Gbmono.EF/Models/BrandCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Gbmono.EF.Models
+{
+    public class BrandCollection
+    {
+        public int BrandCollectionId { get; set; }
+
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
